78d0712600754000c675bb7b96eb636d7f52f918
Bhavin Patel <bhavin_1881@hotmail.com>
2018-11-08 23:32:44 +1300

    Added the Problem1 java class

---
+++ JavaBackAgain/src/com/bhavin/Problem1.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.bhavin;
+
+/**
+ * If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+ * Find the sum of all the multiples of 3 or 5 below 1000.
+ * @author bhavin
+ */
+public class Problem1 {
+    
+    public int sum = 0;
+    
+    public Problem1(){
+        
+    }
+    public static void main(String args[]){
+        
+    }
+}
